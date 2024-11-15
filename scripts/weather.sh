1760c71bf8ef13f51498015275c865cd0f0623e7
banepanel <94392748+xanedarel@users.noreply.github.com>
2024-11-15 12:22:34 +0100

    Update weather.sh

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -9,8 +9,11 @@ function clear_screen() {
 while true; do
 	clear_screen
 	curl -s v2.wttr.in/Lille?0p | sed '2,3d; 6,7d; 18d; 28,37d' | head -n 24;
+ # this is dumb but that way I can reload the weather by pressing enter or anything, and quit using N
 	read varread;
 	if [ "$varread" == "n" ]; then
 		break;
 	fi;
 done
+
+# TODO : make tput reset back
