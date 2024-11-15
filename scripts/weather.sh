030e0b78d63b55cdb8ef993497f8a527be8307ba
banepanel <94392748+xanedarel@users.noreply.github.com>
2024-11-15 12:21:38 +0100

    Update weather.sh

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-#clear
-while true; do clear && curl -s v2.wttr.in/Lille?0p | sed '2,3d; 6,7d; 18d; 28,37d' | head -n 24; read varread; if [ "$varread" == "n" ]; then break; fi; done
+
+function clear_screen() {
+	tput clear
+	tput civis
+	tput home
+}
+
+while true; do
+	clear_screen
+	curl -s v2.wttr.in/Lille?0p | sed '2,3d; 6,7d; 18d; 28,37d' | head -n 24;
+	read varread;
+	if [ "$varread" == "n" ]; then
+		break;
+	fi;
+done
